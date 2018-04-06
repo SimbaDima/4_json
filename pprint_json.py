488e5209b752252f73ef5d9e4aa7a3ac6c3e3515
SimbaDima <37153509+SimbaDima@users.noreply.github.com>
2018-04-06 11:19:51 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,21 +1,21 @@
 import json
 
 
-def load_data(filepath):
+def load_data_from_file(file_path):
 
     try:
-        myFile = open(filepath, "r")
+        my_file = open(file_path, "r")
     except FileNotFoundError:
         return None
-    data = json.load(myFile)
-    myFile.close()
-    return data
+    data_load_from_file = json.load(my_file)
+    my_file.close()
+    return data_load_from_file
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
+def pretty_print_json_in_console(data_load_from_file):
+    print(json.dumps(data_load_from_file, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    data1 = load_data("C:\\Users\\User\\PycharmProjects\\firstTask\\jsonFormat.txt")
-    pretty_print_json(data1)
+    data1 = load_data_from_file("C:\\Users\\User\\PycharmProjects\\firstTask\\jsonFormat.txt")
+    pretty_print_json_in_console(data1)
