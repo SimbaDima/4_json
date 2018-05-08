adc798cb337801b875b76bb534987c7ad6c86292
SimbaDima <37153509+SimbaDima@users.noreply.github.com>
2018-05-08 11:39:06 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,22 +1,22 @@
 import json
 
 
-def loading_data_from_file(file_path):
+def loading_data_from_file(file_path_of_user):
     try:
-        with open(file_path) as file_content_json:
-            data_from_file = json.load(file_content_json)
+        with open(file_path_of_user) as object_of_file:
+            data_format_json = json.load(object_of_file)
     except FileNotFoundError:
         return None
 
-    return data_from_file
+    return data_format_json
 
 
-def pretty_print_json_in_console(data_from_file):
-    print(json.dumps(data_from_file, indent=4, sort_keys=True, ensure_ascii=False))
+def pretty_print_json(data_format_json):
+    print(json.dumps(data_format_json, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == "__main__":
     print("enter path to file:")
     file_path_user = input()
     data_file_user = loading_data_from_file(file_path_user)
-    pretty_print_json_in_console(data_file_user)
+    pretty_print_json(data_file_user)
